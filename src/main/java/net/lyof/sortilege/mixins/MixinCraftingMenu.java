ca6180a965cc0d606084ec1c3b07ba7a06c69387
Lyof <florian.lyof@laposte.net>
2024-03-26 20:31:06 +0100

    Theoretically fixed the compat issue with Polymorph

--- src/main/java/net/lyof/sortilege/mixins/MixinCraftingMenu.java
+++ src/main/java/net/lyof/sortilege/mixins/MixinCraftingMenu.java
@@ -14,11 +14,14 @@ import net.minecraft.world.inventory.CraftingMenu;
 import net.minecraft.world.inventory.ResultContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Optional;
@@ -28,7 +31,7 @@ public class MixinCraftingMenu {
     /**
      * @author Lyof (Sortilege)
      * @reason Locking recipes behind xp levels
-     */
+     *//*
     @Inject(method = "slotChangedCraftingGrid", at = @At("HEAD"), cancellable = true)
     private static void slotChangedCraftingGrid(AbstractContainerMenu menu, Level world, Player player,
                                                   CraftingContainer craftingContainer, ResultContainer resultContainer, CallbackInfo ci) {
@@ -64,5 +67,20 @@ public class MixinCraftingMenu {
 
             ci.cancel();
         }
+    }*/
+
+    @Redirect(method = "slotChangedCraftingGrid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ResultContainer;setRecipeUsed(Lnet/minecraft/world/level/Level;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/item/crafting/Recipe;)Z"))
+    private static boolean lockCrafting(ResultContainer instance, Level world, ServerPlayer player, Recipe recipe) {
+        String recipeid = recipe.getId().toString();
+        int level = player.experienceLevel;
+        Double required = ConfigEntries.xpRequirements.getOrDefault(recipeid, -1.0);
+
+        if (level < required.intValue()) {
+            player.sendSystemMessage(
+                    Component.translatable("sortilege.requires_level").append(" " + required.intValue() + "!")
+                            .withStyle(ChatFormatting.YELLOW), true);
+            world.playSound(player, player.blockPosition(), SoundEvents.AMETHYST_BLOCK_HIT, SoundSource.PLAYERS, 1, 1);
+        }
+        return level >= required.intValue() && instance.setRecipeUsed(world, player, recipe);
     }
 }

da77413fb2779b137ea7e0a7a418a9a4ba0e483f
Lyof <florian.lyof@laposte.net>
2024-05-23 19:01:39 +0200

    forgot a mixin

--- src/main/java/net/lyof/sortilege/mixins/MixinAnvilMenu.java
+++ src/main/java/net/lyof/sortilege/mixins/MixinAnvilMenu.java
@@ -1,6 +1,7 @@
 package net.lyof.sortilege.mixins;
 
 import net.lyof.sortilege.configs.ConfigEntries;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.inventory.DataSlot;
 import org.spongepowered.asm.mixin.Final;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AnvilMenu.class)
 public class MixinAnvilMenu {
@@ -17,5 +19,10 @@ public class MixinAnvilMenu {
     @Inject(method = "createResult", at = @At("RETURN"))
     public void noAnvilCost(CallbackInfo ci) {
         if (ConfigEntries.noXPAnvil) this.cost.set(0);
+    }
+
+    @Inject(method = "mayPickup", at = @At("RETURN"), cancellable = true)
+    public void canTakeFix(Player player, boolean present, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
     }
 }

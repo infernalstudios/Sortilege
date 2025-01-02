c7516c712c85605f075d81ca4565ccb5189356b9
Jonathing <me@jonathing.me>
2025-01-02 15:57:25 -0500

    Re-add deleted XPHelper members from #21

--- src/main/java/net/lyof/sortilege/utils/XPHelper.java
+++ src/main/java/net/lyof/sortilege/utils/XPHelper.java
@@ -1,8 +1,16 @@
 package net.lyof.sortilege.utils;
 
 import net.lyof.sortilege.capabilities.entity.EntityXpStorage;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.util.FakePlayerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class XPHelper {
     public static boolean hasXP(Player player, int amount) {
@@ -40,4 +48,27 @@ public class XPHelper {
         EntityXpStorage.ifPresent(entity, storage -> storage.setXp(xp, onlyIfZero));
     }
 
+
+    /* DEPRECATED MEMBERS KEPT FOR BIN COMPAT */
+
+    // DO NOT USE!!! Use one of the above methods instead that uses the EntityXpStorage capability instead
+    @Deprecated(forRemoval = true)
+    public static final Map<String, Integer> XP_SAVES = new HashMap<>();
+
+    // DO NOT USE!!! Use getTotalxp(Player) instead
+    @Deprecated(forRemoval = true)
+    public static int getTotalxp(int level, float progress, ServerLevel server) {
+        var player = FakePlayerFactory.getMinecraft(server);
+        player.experienceLevel = level;
+        player.experienceProgress = progress;
+        return getTotalxp(player);
+    }
+
+    @Deprecated(forRemoval = true)
+    public static void dropxpPinata(Level world, double x, double y, double z, int amount) {
+        // server will handle sending the xp to client
+        if (world.isClientSide) return;
+
+        ExperienceOrb.award((ServerLevel) world, new Vec3(x, y, z), amount);
+    }
 }

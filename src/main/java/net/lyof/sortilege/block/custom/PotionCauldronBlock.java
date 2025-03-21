658f132f089e5f8a3f1163f77b2cf59d0dccb477
Lyof429 <florian.lyof@laposte.net>
2025-03-21 12:03:57 +0100

    cauldron stuff

--- src/main/java/net/lyof/sortilege/block/custom/PotionCauldronBlock.java
+++ src/main/java/net/lyof/sortilege/block/custom/PotionCauldronBlock.java
@@ -3,13 +3,27 @@ package net.lyof.sortilege.block.custom;
 import net.lyof.sortilege.block.entity.PotionCauldronBlockEntity;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.CauldronBlock;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -26,14 +40,27 @@ public class PotionCauldronBlock extends LeveledCauldronBlock implements BlockEn
         };
 
         public static void register() {
+            INSTANCE.put(Items.GLASS_BOTTLE, (state, world, pos, player, hand, stack) -> {
+                if (world.getBlockEntity(pos) instanceof PotionCauldronBlockEntity cauldron) {
+                    stack.decrement(1);
+                    player.giveItemStack(PotionUtil.setPotion(Items.POTION.getDefaultStack(), cauldron.potion));
 
+                    player.incrementStat(Stats.USE_CAULDRON);
+                    player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+                    world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    world.emitGameEvent(null, GameEvent.FLUID_PICKUP, pos);
+                }
+
+                LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
+                return ActionResult.success(world.isClient());
+            });
         }
     }
 
 
 
     public PotionCauldronBlock(Settings settings) {
-        super(settings, precipitation -> true, Behavior.INSTANCE);
+        super(settings.ticksRandomly(), precipitation -> true, Behavior.INSTANCE);
     }
 
     @Override
@@ -46,5 +73,15 @@ public class PotionCauldronBlock extends LeveledCauldronBlock implements BlockEn
         super.onSyncedBlockEvent(state, world, pos, type, data);
         BlockEntity entity = world.getBlockEntity(pos);
         return entity != null && entity.onSyncedBlockEvent(type, data);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        super.randomTick(state, world, pos, random);
+
+        if (world.random.nextFloat() < 0.5 && state.get(LEVEL) != 3) {
+            BlockState blockState = state.cycle(LEVEL);
+            world.setBlockState(pos, blockState);
+        }
     }
 }
--- src/main/java/net/lyof/sortilege/block/entity/PotionCauldronBlockEntity.java
+++ src/main/java/net/lyof/sortilege/block/entity/PotionCauldronBlockEntity.java
@@ -4,9 +4,13 @@ import net.lyof.sortilege.block.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.math.BlockPos;
 
 public class PotionCauldronBlockEntity extends BlockEntity {
+    public Potion potion = Potions.STRENGTH;
+
     public PotionCauldronBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.POTION_CAULDRON, pos, state);
     }

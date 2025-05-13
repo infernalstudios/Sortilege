01594c25f935243c9260cf84420a8b3900c4b6e3
Lyof <florian.lyof@laposte.net>
2025-05-13 15:23:34 +0200

    curse of ignorance is now a treasure enchant

--- src/main/java/net/lyof/sortilege/enchant/staff/CurseStaffEnchantment.java
+++ src/main/java/net/lyof/sortilege/enchant/staff/CurseStaffEnchantment.java
@@ -1,12 +1,17 @@
 package net.lyof.sortilege.enchant.staff;
 
 public class CurseStaffEnchantment extends StaffEnchantment {
-    public CurseStaffEnchantment(Rarity pRarity) {
-        super(pRarity, 1);
+    public CurseStaffEnchantment(Rarity rarity) {
+        super(rarity, 1);
     }
 
     @Override
     public boolean isCursed() {
+        return true;
+    }
+
+    @Override
+    public boolean isTreasure() {
         return true;
     }
 }

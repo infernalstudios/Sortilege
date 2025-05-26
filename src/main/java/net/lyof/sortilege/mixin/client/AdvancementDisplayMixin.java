5342d3d5e886be10607a8940666f4ace3eaa6613
Lyof <florian.lyof@laposte.net>
2025-05-26 19:24:18 +0200

    optimized imports, packet time

--- src/main/java/net/lyof/sortilege/mixin/client/AdvancementDisplayMixin.java
+++ src/main/java/net/lyof/sortilege/mixin/client/AdvancementDisplayMixin.java
@@ -1,16 +1,10 @@
 package net.lyof.sortilege.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.lyof.sortilege.Sortilege;
 import net.minecraft.advancement.AdvancementDisplay;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AdvancementDisplay.class)

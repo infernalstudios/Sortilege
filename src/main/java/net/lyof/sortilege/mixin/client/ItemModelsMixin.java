d2d4f7629d5e23447e03389d59be69684b67f670
Lyof <florian.lyof@laposte.net>
2025-05-30 14:43:43 +0200

    added a renderer cache

--- src/main/java/net/lyof/sortilege/mixin/client/ItemModelsMixin.java
+++ src/main/java/net/lyof/sortilege/mixin/client/ItemModelsMixin.java
@@ -5,12 +5,14 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.lyof.sortilege.Sortilege;
 import net.lyof.sortilege.item.custom.potion.AntidotePotionItem;
 import net.lyof.sortilege.item.custom.potion.CustomPotionData;
+import net.lyof.sortilege.item.custom.potion.IPotionShenanigans;
 import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
 import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.PotionItem;
@@ -23,13 +25,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mixin(ItemModels.class)
 public class ItemModelsMixin {
+    @Unique private static final Map<ItemStack, BakedModel> CACHE = new HashMap<>();
+
     @Shadow @Final private BakedModelManager modelManager;
 
     @WrapMethod(method = "getModel(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/client/render/model/BakedModel;")
     public BakedModel getCustomModel(ItemStack stack, Operation<BakedModel> original) {
         if (!(stack.getItem() instanceof PotionItem) || stack.getItem() instanceof AntidotePotionItem) return original.call(stack);
+        if (CACHE.containsKey(stack)) return CACHE.get(stack);
 
         Identifier potion = Registries.POTION.getId(PotionUtil.getPotion(stack));
 
@@ -40,7 +48,9 @@ public class ItemModelsMixin {
         BakedModel model = this.modelManager.getModel(new ModelIdentifier(Identifier.of(potion.getNamespace(),
                 path + potion.getPath()), "inventory"));
         if (model == this.modelManager.getMissingModel())
-            return original.call(stack);
+            model = original.call(stack);
+
+        CACHE.put(stack, model);
         return model;
     }
 }

509ee11660df7cdbfe1d43e724bdfcaa757854ea
Lyof <florian.lyof@laposte.net>
2026-06-20 16:10:18 +0200

    origins resource staff!

---
+++ src/main/java/net/lyof/sortilege/item/custom/staff/OriginsStaffItem.java
@@ -0,0 +1,110 @@
+package net.lyof.sortilege.item.custom.staff;
+
+import com.google.gson.JsonObject;
+import dev.onyxstudios.cca.api.v3.component.ComponentAccess;
+import io.github.apace100.apoli.command.PowerCommand;
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import io.github.apace100.apoli.power.Power;
+import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.VariableIntPower;
+import io.github.apace100.origins.OriginsClient;
+import io.github.apace100.origins.origin.Origin;
+import io.wispforest.affinity.component.AffinityComponents;
+import io.wispforest.affinity.component.PlayerAethumComponent;
+import net.lcc.sollib.core.Identifier;
+import net.lcc.sollib.platform.Dependency;
+import net.lyof.sortilege.Sortilege;
+import net.lyof.sortilege.enchant.staff.ElementalStaffEnchantment;
+import net.lyof.sortilege.item.custom.AStaffItem;
+import net.lyof.sortilege.item.staff.IStaffEntryReader;
+import net.lyof.sortilege.item.staff.StaffEntry;
+import net.lyof.sortilege.item.staff.entry.ValueCost;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+import java.util.Set;
+
+public class OriginsStaffItem extends AStaffItem {
+    @Dependency(mod = "origins:resource")
+    public static class Reader implements IStaffEntryReader {
+        @Override
+        public StaffEntry.Cost readCost(JsonObject json) {
+            return new Cost().read(json);
+        }
+
+        @Override
+        public AStaffItem make(StaffEntry entry) {
+            return new OriginsStaffItem(entry, new Properties());
+        }
+    }
+
+    protected static class Cost extends ValueCost {
+        protected ResourceLocation resource;
+
+        @Override
+        public Cost read(JsonObject json) {
+            super.read(json);
+            this.resource = Identifier.of(GsonHelper.getAsString(json, "resource"));
+            return this;
+        }
+
+        public ResourceLocation getResourceIdentifier() {
+            return resource;
+        }
+    }
+
+
+    protected final Cost cost;
+
+    public OriginsStaffItem(StaffEntry entry, Properties properties) {
+        super(entry, properties);
+        this.cost = (Cost) this.getEntry().getCost();
+    }
+
+    public PowerType<VariableIntPower> getPower(Player player) {
+        for (PowerType<?> powerType : PowerHolderComponent.KEY.get(player).getPowerTypes(true)) {
+            if (powerType.getIdentifier().equals(cost.getResourceIdentifier())
+                    && powerType.get(player) instanceof VariableIntPower)
+                return (PowerType<VariableIntPower>) powerType;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean hasResource(ItemStack stack, Player player) {
+        PowerType<VariableIntPower> powerType = getPower(player);
+        if (powerType == null) return false;
+
+        return powerType.get(player).getValue() >= this.getCost(stack, player, cost.getValue());
+    }
+
+    @Override
+    public void consumeResource(ItemStack stack, Player player) {
+        PowerType<VariableIntPower> powerType = getPower(player);
+        if (powerType == null) return;
+
+        powerType.get(player).setValue(powerType.get(player).getValue() - this.getCost(stack, player, cost.getValue()));
+    }
+
+    @Override
+    public void appendTooltipCosts(ItemStack stack, Player player, List<Component> tooltip) {
+        super.appendTooltipCosts(stack, player, tooltip);
+
+        if (this.getCost(stack, player, cost.getValue()) > 0) {
+            PowerType<VariableIntPower> powerType = getPower(player);
+            if (powerType == null) return;
+
+            tooltip.add(Component.translatable("sortilege.staff.cost.origins_resource",
+                    this.getCost(stack, player, cost.getValue()),
+                    powerType.getName().withStyle(ChatFormatting.GRAY)
+            ).withStyle(ChatFormatting.DARK_GRAY));
+        }
+    }
+}

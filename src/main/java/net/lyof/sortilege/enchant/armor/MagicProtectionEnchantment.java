d71bcb139103260993d232452c008f91cacd276d
Lyof <florian.lyof@laposte.net>
2026-06-27 15:53:57 +0200

    magic prot should now work against all modded magic damage

--- src/main/java/net/lyof/sortilege/enchant/armor/MagicProtectionEnchantment.java
+++ src/main/java/net/lyof/sortilege/enchant/armor/MagicProtectionEnchantment.java
@@ -1,7 +1,14 @@
 package net.lyof.sortilege.enchant.armor;
 
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
+import net.lcc.sollib.core.Identifier;
 import net.lyof.sortilege.setup.ModConfig;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
@@ -9,13 +16,15 @@ import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.ProtectionEnchantment;
 
 public class MagicProtectionEnchantment extends Enchantment {
+    public static final TagKey<DamageType> IS_MAGIC = TagKey.create(Registries.DAMAGE_TYPE, Identifier.of("c", "is_magic"));
+
     public MagicProtectionEnchantment(Enchantment.Rarity rarity) {
         super(rarity, EnchantmentCategory.ARMOR, EquipmentSlot.values());
     }
 
     @Override
     public int getDamageProtection(int level, DamageSource source) {
-        if (source.is(DamageTypes.MAGIC) || source.is(DamageTypes.INDIRECT_MAGIC))
+        if (source.is(DamageTypeTags.WITCH_RESISTANT_TO) || source.is(IS_MAGIC))
             return level * 3;
         return 0;
     }
